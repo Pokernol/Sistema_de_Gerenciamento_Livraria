ef28998580c8b853f8dd3be349332d928d3d52c3
Leonardo Vinicius <100011077+Pokernol@users.noreply.github.com>
2024-05-14 00:59:27 -0300

    Primeiro commit, inicio projeto livraria com criação de entidades

---
+++ src/main/java/br/com/fatecmogidascruzes/entity/Livro.java
@@ -0,0 +1,25 @@
+package br.com.fatecmogidascruzes.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Livro {
+    private long id;
+    private String titulo;
+    private String sinopse;
+    private String autor;
+    private String editora;
+    private String genero;
+    private String idioma;
+    private BigDecimal preco;
+    private LocalDate anoDePublicacao;
+}
---
+++ src/main/java/br/com/fatecmogidascruzes/entity/Usuario.java
@@ -0,0 +1,21 @@
+package br.com.fatecmogidascruzes.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Usuario {
+    private long id;
+    private String nome;
+    private String Email;
+    private String telefone;
+    private LocalDate dataNascimento;
+}
