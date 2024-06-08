5456e29386af0a03923342bfd6db2af2fb150016
Leonardo Vinicius <leonardo@poke25@gmail.com>
2024-06-08 13:55:29 -0300

    retirado imports desnecessários

--- src/main/java/br/com/fatecmogidascruzes/service/ClienteService.java
+++ src/main/java/br/com/fatecmogidascruzes/service/ClienteService.java
@@ -1,8 +1,6 @@
 package br.com.fatecmogidascruzes.service;
 
 import br.com.fatecmogidascruzes.model.entity.Cliente;
-import br.com.fatecmogidascruzes.model.entity.Livro;
-
 import java.util.List;
 
 public interface ClienteService {
